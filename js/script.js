ab55e295fb053756de324e3823c52fde3c07362d
paulpollob <paulpollob71@gmail.com>
2022-08-30 20:45:54 +0600

    added spinner

--- js/script.js
+++ js/script.js
@@ -10,6 +10,23 @@ const load_phones = async (phoneName) => {
 const placeData = data => {
     const phones = document.getElementById("phones");
     phones.innerHTML = ``;
+    if(data.length === 0)
+    {
+        document.getElementById("no-result").classList.remove('d-none');
+        document.getElementById("buttonShowMore").classList.add('d-none');
+    }
+    else {
+        document.getElementById("no-result").classList.add('d-none');
+        if(data.length>10)
+        {
+            data = data.slice(0, 10);
+            document.getElementById("buttonShowMore").classList.remove('d-none');
+        }
+        else
+        { 
+            document.getElementById("buttonShowMore").classList.add('d-none');
+        }
+    }
     for (const phone of data) {
         console.log(phone);
         const div = document.createElement('div');
@@ -24,12 +41,38 @@ const placeData = data => {
         </div>`;
         phones.append(div);
     }
+    toggleSpinner(false);
 }
 
 
 document.getElementById('search').addEventListener('click', function(){
-    console.log("Hare Krishna from button" );
+    toggleSpinner(true);
     const phoneName = document.getElementById('input').value;
     load_phones(phoneName);
+});
+
+
+const toggleSpinner = isSpinner=>{
+    const spinner = document.getElementById('spinner');
+    if(isSpinner)
+    {
+        document.getElementById("spinner").classList.remove("d-none");
+    }
+    else
+    {
+        document.getElementById("spinner").classList.add("d-none");
+    }
+}
+
+ 
+
+
+document.getElementById('input').addEventListener('keydown', function(event){
+    if(event.key === "Enter")
+    {
+        console.log("Enter button clicked");
+    }
+    // const phoneName = document.getElementById('input').value;
+    // load_phones(phoneName);
 });
  
