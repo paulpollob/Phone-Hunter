0aba1da190d5679a346a5a0155594c6ff5b625c8
paulpollob <paulpollob71@gmail.com>
2022-08-31 12:07:48 +0600

    show more button complete

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 console.log("Hare Krishna");
+let data;
+
 const load_phones = async (phoneName) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${phoneName}`; 
     const res = await fetch(url);
-    const data = await res.json();
-    placeData(data.data);
+    data = await res.json();
+    data = data.data;
+    placeData(data, true);
 }
 
 
-const placeData = data => {
+const placeData = (data, isSliceNmbr) => {
     const phones = document.getElementById("phones");
     phones.innerHTML = ``;
     if(data.length === 0)
@@ -19,7 +22,9 @@ const placeData = data => {
         document.getElementById("no-result").classList.add('d-none');
         if(data.length>10)
         {
-            data = data.slice(0, 10);
+            if(isSliceNmbr === true) {
+                data = data.slice(0, 10);
+            }
             document.getElementById("buttonShowMore").classList.remove('d-none');
         }
         else
@@ -64,6 +69,12 @@ const toggleSpinner = isSpinner=>{
     }
 }
 
+
+
+document.getElementById("buttonShowMore").addEventListener("click", function(){
+    placeData(data, false);
+});
+
  
 
 
@@ -76,3 +87,13 @@ document.getElementById('input').addEventListener('keydown', function(event){
     // load_phones(phoneName);
 });
  
+
+
+
+
+
+
+// tast
+// enter handler
+// add description
+// show descript using modal
